bcc7ea737cbb3214d0d90b21ead00e8bbb561887
Sam Zhao <zhao6518@gmail.com>
2017-01-08 12:26:37 -0800

    stuff

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -39,8 +39,8 @@ exports.handle = (client) => {
     },
 
     prompt() {
-      client.addImageResponse('https://pbs.twimg.com/profile_images/378800000750533027/918c17d0f03cc65bac507ca2127b23b6.jpeg')
       client.addResponse('greeting')
+      client.addImageResponse('https://pbs.twimg.com/profile_images/378800000750533027/918c17d0f03cc65bac507ca2127b23b6.jpeg', 'Hey there!')
       client.updateConversationState({
         greetingSent: true
       })
